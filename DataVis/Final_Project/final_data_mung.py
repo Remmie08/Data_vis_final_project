8c2ddf52117bc3ce0eaf61ad33d3ea9f1e55725e
Reid Maury <reidmaury@MacBook-Air-100.local>
2025-11-07 15:16:54 -0500

    added over time chart for page 2 graphs

--- DataVis/Final_Project/final_data_mung.py
+++ DataVis/Final_Project/final_data_mung.py
@@ -2,13 +2,11 @@ import pandas as pd
 import numpy as np
 import os
 
-
+#GOALS SCORED BY TEAMS
 goals_df = pd.read_csv(os.path.join("csv_files", "PremierLeague24_25(Goals).csv"), encoding="latin-1")
-
 goals_df["Goals"] = 1
 teams_goals = goals_df.groupby("Team")["Goals"].sum().reset_index()
 player_goals = goals_df.groupby(["Team", "Scorer"])["Goals"].sum().reset_index()
-
 teams_goals.to_csv("PremierLeague24_25(Teams_Goals).csv", index=False)
 player_goals.to_csv("PremierLeague24_25(Player_Goals).csv", index=False)
 
